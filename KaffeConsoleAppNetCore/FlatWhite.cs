01fc0503845c3393db60de7fd7269a67e15b5a6e
Lasse Henriksen <lass5676@edu.easj.dk>
2019-02-01 13:47:52 +0100

    Kaffe opgave delvist fuldført

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using kaffe;
+
+namespace KaffeConsoleAppNetCore
+{
+    public class FlatWhite : Kaffe, Imælk
+    {
+        public FlatWhite(int rabat) :base(rabat)
+        {
+            //Rabat = rabat;
+        }
+
+        public int MlMælk()
+        {
+            return 120;
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public override string Styrke()
+        {
+            return "mild";
+        }
+    }
+}
--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using kaffe;
 using System;
+using System.Collections.Generic;
 
 namespace KaffeConsoleAppNetCore
 {
@@ -8,7 +9,28 @@ namespace KaffeConsoleAppNetCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Cortado cortado = new Cortado();
+            Latte latte = new Latte();
+            FlatWhite fw = new FlatWhite(12);
 
+            List<Imælk> liste = new List<Imælk>();
+            liste.Add(cortado);
+            liste.Add(latte);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine("før cast" + item.MlMælk());
+                if (item is Latte)
+                {
+                    Latte l = item as Latte;
+                    Console.WriteLine("Latte styrke" + l.Styrke());
+                }
+                else if (item is Cortado)
+                {
+                    Cortado c = item as Cortado;
+                }
+               
+            }
             
 
            
